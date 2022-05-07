ce340d7cb62b2f7e80f6116caea316ae186ef560
udonetsm <donetsmaksimm@yandex.ru>
2022-05-07 13:57:38 +0300

    subscriber set data to db

---
+++ db/db.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+
+	"github.com/udonetsm/help/helper"
+	"github.com/udonetsm/help/models"
+	mod "github.com/udonetsm/publisher/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func connStr(path string) string {
+	conn_data := models.Postgres_conf{}
+	conn_data = conn_data.StoreConf(path)
+	return conn_data.Dbname + conn_data.Dbpassword + conn_data.SslMode +
+		conn_data.Dbhost + conn_data.Dbport + conn_data.Dbuser
+}
+
+func sqlDb() *sql.DB {
+	defer helper.PanicCapture("sqlDb")
+	path := helper.Home() + "/.confs/conn_config.yaml"
+	connstr := connStr(path)
+	sdb, err := sql.Open("pgx", connstr)
+	helper.Errors(err, "sqlopen")
+	sdb.SetMaxIdleConns(5)
+	sdb.SetMaxOpenConns(6)
+	sdb.SetConnMaxIdleTime(2)
+	sdb.SetConnMaxLifetime(3)
+	return sdb
+}
+
+func gormDb() *gorm.DB {
+	sdb := sqlDb()
+	defer helper.PanicCapture("gormDb")
+	gdb, err := gorm.Open(postgres.New(postgres.Config{Conn: sdb}), &gorm.Config{})
+	helper.Errors(err, "gormOpen")
+	return gdb
+}
+
+func New(data []byte, timestamp int64) {
+	sdb := sqlDb()
+	order := mod.Order{}
+	log.Println(json.Unmarshal(data, &order))
+	_, err := sdb.Query("insert into messages(id, orderjson, pubdate) values($1, $2, $3)", order.Order_id, string(data), timestamp)
+	if err != nil {
+		_, err = sdb.Query("update messages set orderjson=$1 and pubdate=$2 where id=$3", string(data), timestamp, order.Order_id)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func GetMax() (maxpubdate int64) {
+	sdb := sqlDb()
+	err := sdb.QueryRow("select max(pubdate) from messages").Scan(&maxpubdate)
+	helper.Errors(err, "GetMax")
+	return
+}
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"nats/db"
+	"time"
+
+	"github.com/nats-io/stan.go"
+)
+
+func main() {
+	ConnectAndSubscribe("_", "test-cluster", "nats://127.0.0.1:4222", "orders")
+}
+
+func ConnectAndSubscribe(clientid, clusterid, url, sub string) {
+	fmt.Print("Connecting to nats streaming server --> ")
+	sc, err := stan.Connect(clusterid, clientid, stan.NatsURL(url),
+		stan.SetConnectionLostHandler(func(c stan.Conn, err error) {
+			log.Println("Disconnected")
+		}))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println("[SUCCESS]")
+	Subscribe(sub, sc)
+}
+
+func Subscribe(sub string, sc stan.Conn) {
+	lastTimeWrittenData := db.GetMax()
+	delta := time.Now().UnixNano() - lastTimeWrittenData
+	_, err := sc.QueueSubscribe(sub, "que", func(msg *stan.Msg) {
+		db.New(msg.Data, msg.Timestamp)
+	}, stan.StartAtTimeDelta(time.Duration(delta)))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Scanln()
+}
